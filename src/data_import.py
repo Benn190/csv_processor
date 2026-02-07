71fc124bdd9b3f8310b48223f7011588bf91b7b3
Benn190 <benhaine99@gmail.com>
2026-02-07 11:39:18 +0000

    write json makeup to file as well

--- src/data_import.py
+++ src/data_import.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 import time
+import json
 
 # makeup api provides data on many makeup items. Including details such as product type, price, and available colours.
 def get_makeup_data():
@@ -15,7 +16,7 @@ def get_makeup_data():
         
         # check if request was successful (200 OK status code)
         if response.status_code == 200:
-            print(time.strftime('%x %X') + " - Request successful")
+            print(time.strftime('%x %X') + " - Request successful, status code: " + str(response.status_code))
             makeup_items = response.json()
             return makeup_items
         else:
@@ -29,7 +30,16 @@ def main():
     posts = get_makeup_data()
     
     if posts:
+        
+        print(time.strftime('%x %X') + " - Writing JSON Makeup data to .json file")
+        with open('data/raw/makeup.json', 'w') as f:
+            json.dump(posts, f)
+        
+        # format json from API as a pandas dataframe
         df = pd.json_normalize(posts)
+        print(time.strftime('%x %X') + " - Converting API JSON response to pandas dataframe")
+        
+        # convert dataframe and save as csv
         df.to_csv('data/raw/makeup.csv', encoding='utf-8', index=False)
         print(time.strftime('%x %X') + " - Makeup data written to CSV")
 
---
+++ src/processor.py
@@ -0,0 +1,5 @@
+def process_makeup_json(makeup_json):
+    a=1
+    
+def process_makeup_csv(makeup_csv):
+    a=1
